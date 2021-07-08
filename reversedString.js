a8c603a7c9791a432240bdf328c04873f9b20803
isa <sumerisa5@gmail.com>
2021-07-08 10:03:28 +0300

    reversed string added

---
+++ reversedString.js
@@ -0,0 +1,23 @@
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverseString("hello"));
+
+
+// // function reverseString(str) {
+// //     let arr=str.split("").reverse().join("");
+
+// //     return arr
+// // }
+// // console.log(reverseString("hello"));
+
+
+// function reverseString(str) {
+//     let arr= str.split("");
+//     for (i=length.arr; i>0; i--){
+//         let str1 ="";
+//         str+=arr[length-i];
+//     }
+//     return str1
+// }
+// console.log(reverseString("hello"));
