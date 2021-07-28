36eaf948109731b57fe47ee6567fd75dfa1a0a72
isa <sumerisa5@gmail.com>
2021-07-29 02:40:25 +0300

    small changes

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,11 @@
-/* function fizzbuzz(n){
+function fizzbuzz(n){
     for (let i = 1; i < n; i++) {
         console.log (` ${i} : ${ i % 3 === 0 && i % 5 === 0 ? 'fizz-buzz' :
         ( i % 5 === 0 ? 'buzz' :  ( i % 3 === 0  ? 'fizz' : i ) ) }`)
     }
 }
 let n = prompt("Please enter a number")
-fizzbuzz(n) */
+fizzbuzz(n) ;
 
 /* function fizzbuzz(n){
     for (let i = 1; i < n; i++) {
