ec38bdc5592f0ebc9d7cb64cd02bdeb6cde03e1d
isa <sumerisa5@gmail.com>
2021-08-01 23:07:41 +0300

    switch case added

---
+++ switch.js
@@ -0,0 +1,25 @@
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch (s[0]) {
+        case ('a' || 'e' || 'o' || 'i' || 'u'):
+            letter = 'A';
+            break;
+
+        case ('b' || 'c' || 'd' || 'f' || 'g'):
+            letter = 'B';
+            break;
+
+        case ('h' || 'j' || 'k' || 'l' || 'm'):
+            letter = 'C';
+            break;
+
+        case ('z' || 'n' || 'p' || 'q' || 'r' || 's' || 't' || 'v' || 'w' || 'x' || 'y'):
+            letter = 'D';
+
+    }
+
+    return (letter);
+}
+
+getLetter(s="asdansdpn")
