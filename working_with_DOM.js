4a5fe25e1f53cb233f4e02a0634a91dea4e6c330
isa <sumerisa5@gmail.com>
2021-08-03 12:13:44 +0300

    getRandomColor Added

--- working_with_DOM.js
+++ working_with_DOM.js
@@ -1,4 +1,30 @@
 let greeting = document.querySelector("#greeting")
-greeting.addEventListener("click", function () {
-    console.log("tıklandı")
-})
+greeting.addEventListener("click", domClick )
+
+let renk = document.querySelector("#renk")
+renk.addEventListener("mouseover", domOver)
+
+function domOver () {
+    renk.style.color =getRandomColor()
+    greeting.style.color = getRandomColor()
+}
+
+function domClick() {
+    this.style.color = getRandomColor()
+}
+
+function getRandomColor(){
+    let letters= "0123456789ABCDEF";
+    var color ="#"
+    for (let i=0; i<6; i++) {
+        color += letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+
+
+
+
+
+
+
